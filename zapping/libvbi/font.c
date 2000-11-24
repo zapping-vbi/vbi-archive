1f363ca4d285b5103be6af069dd3221c17a3ed03
Iñaki García Etxebarria <garetxe@users.sourceforge.net>
2000-11-24 23:27:49 +0000

    Russian glyphs added

--- zapping/libvbi/font.c
+++ zapping/libvbi/font.c
@@ -1,3 +1,10 @@
+#include "font.h"
+
 #define static /* I want them global */
+#ifndef FONTS_RUSSIAN
 #include "font1.xbm"
 #include "font2.xbm"
+#else /* use russian glyphs instead */
+#include "font3.xbm"
+#include "font4.xbm"
+#endif
--- zapping/libvbi/font.h
+++ zapping/libvbi/font.h
@@ -1,6 +1,23 @@
 #ifndef FONT_H
 #define FONT_H
 
+/* Uncomment this line to get Cyrillic Russian fonts */
+// #define FONTS_RUSSIAN 1
+/*
+  FIXME: This is a hot fix, we should allow dynamic switching between
+  fonts, not difficult once we have the glyphs ;-)
+*/
+/* Replace the latin glyphs with the russian ones */
+#ifdef FONTS_RUSSIAN
+#define font1_bits font3_bits
+#define font2_bits font4_bits
+#define font1_width font3_width
+#define font1_height font3_height
+#define font2_width font4_width
+#define font2_height font4_height
+#endif
+
+
 #include "fontsize.h"	/* the #defines from font?.xbm */
 
 #if font1_width != font2_width || font1_height != font2_height
