358dee679c30d260f0d621cd6744907c5c5c08a9
Iñaki García Etxebarria <garetxe@users.sourceforge.net>
2001-03-02 19:06:07 +0000

    Updated

--- zapping/plugins/lirc/lirc.c
+++ zapping/plugins/lirc/lirc.c
@@ -1,5 +1,5 @@
-/* Screenshot saving plugin for Zapping
- * Copyright (C) 2000 Iñaki García Etxebarria
+/* LIRC plugin for Zapping
+ * Copyright (C) 2001 Marco Pfattner
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
