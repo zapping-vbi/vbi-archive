6d69cb6d57d28ab8aeaf3354340d5e74c48970ab
Iñaki García Etxebarria <garetxe@users.sourceforge.net>
2001-04-01 20:37:11 +0000

    added

---
+++ zapping/plugins/screenshot/screenshot_strings.c
@@ -0,0 +1,10 @@
+/*
+ * Archivo de cadenas traducibles generado por Glade.
+ * Añada este archivo a su POTFILES.in de su proyecto.
+ * NO lo compile como parte de su aplicación.
+ */
+
+gchar *s = N_("window1");
+gchar *s = N_("Select here the directory where screenshots will be saved");
+gchar *s = N_("Select directory to save screenshots");
+gchar *s = N_("Quality of the compressed image:");
