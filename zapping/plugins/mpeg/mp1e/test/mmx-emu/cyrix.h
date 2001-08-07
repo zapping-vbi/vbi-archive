60bca4bb578279a60600e3d4dd4c869c0e1a1d44
Iñaki García Etxebarria <garetxe@users.sourceforge.net>
2001-08-07 22:55:17 +0000

    removed files; now in rte module

--- zapping/plugins/mpeg/mp1e/test/mmx-emu/cyrix.h
+++
@@ -1,6 +0,0 @@
-/*
- *  Cyrix MMX functions
- */
-
-extern ifunc paveb, pdistib, pmachriw, pmagw, cyrix_pmulhrw, pmulhriw;
-extern ifunc pmvzb, pmvnzb, pmvlzb, pmvgezb, psubsiw, paddsiw;
--- zapping/plugins/mpeg/mp1e/test/mmx-emu/sse_int.h
+++
@@ -1,9 +0,0 @@
-/*
- *  SSE integer functions
- */
-
-extern ifunc maskmovq, movntq, pavgb, pavgw;
-extern ifunc pextrw, pinsrw, pmaxsw, pmaxub, pminsw, pminub;
-extern ifunc pmovmskb, pmulhuw, psadbw, pshufw;
-extern ifunc sse_prefetch, prefetchnta, prefetchnt0, prefetchnt1, prefetchnt2;
-extern ifunc grp0FAE;
