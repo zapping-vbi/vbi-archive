c8d1bea387a1d71343e0e9ac47141170306e3b41
Michael H. Schimek <mschimek@users.sourceforge.net>
2002-04-02 20:33:52 +0000

    Release 0.6.4

--- web/prepare_web.sh
+++ web/prepare_web.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#$Id: prepare_web.sh,v 1.1 2002-03-16 16:29:28 mschimek Exp $
+#$Id: prepare_web.sh,v 1.2 2002-04-02 20:33:52 mschimek Exp $
 
 cd /home/groups/z/za/zapping
 chmod ug=rwX,o-rwx . -R
@@ -7,8 +7,8 @@ umask 007
 cvs -z3 update -dP
 cvs -z3 -d:pserver:anonymous@cvs.zapping.sourceforge.net:/cvsroot/zapping co zapping/ChangeLog
 mv zapping/ChangeLog htdocs/
-rm -dfR zapping
-chmod a+rX . htdocs cgi-bin
+rm -fR zapping
+chmod a+rX . htdocs
 cd htdocs
 chmod a+r *.php *.inc *.html *.jpeg *.gif *.png bookmark.ico rescd.zip
 for i in images_* screenshots; do
