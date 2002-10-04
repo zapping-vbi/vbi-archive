a299a00b801de91457a0e4f0f4b3ae790787c7b3
Michael H. Schimek <mschimek@users.sourceforge.net>
2002-10-04 17:46:45 +0000

    *** empty log message ***

--- web/prepare_web.sh
+++ web/prepare_web.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#$Id: prepare_web.sh,v 1.4 2002-10-04 17:42:58 mschimek Exp $
+#$Id: prepare_web.sh,v 1.5 2002-10-04 17:46:45 mschimek Exp $
 
 cd /home/groups/z/za/zapping
 chmod ug=rwX,o-rwx . -R
@@ -15,5 +15,5 @@ chmod a+r *.php *.inc *.html *.jpeg *.gif *.png bookmark.ico rescd.zip
 for i in images_* screenshots; do
   find $i -name "CVS" -prune -o -exec chmod a+rX '{}' ';'
 done
-chmod a+r doc -R
+chmod a+rX doc -R
 cd -
