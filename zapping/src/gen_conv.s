4fca2db43b18116c7d4293b52f9ebbf102e9b80b
Michael H. Schimek <mschimek@users.sourceforge.net>
2002-11-15 03:10:30 +0000

    Replaced /**/ remarks, old as don't like it.

--- zapping/src/gen_conv.s
+++ zapping/src/gen_conv.s
@@ -1,24 +1,24 @@
-/*
- *  Zapping (TV viewer for the Gnome Desktop)
- *  SWAR color conversion routines
- *
- *  Copyright (C) 2001 Michael H. Schimek <mschimek@users.sf.net>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
+;
+;   Zapping (TV viewer for the Gnome Desktop)
+;   SWAR color conversion routines
+; 
+;   Copyright (C) 2001 Michael H. Schimek <mschimek@users.sf.net>
+; 
+;   This program is free software; you can redistribute it and/or modify
+;   it under the terms of the GNU General Public License version 2 as
+;   published by the Free Software Foundation.
+; 
+;   This program is distributed in the hope that it will be useful,
+;   but WITHOUT ANY WARRANTY; without even the implied warranty of
+;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;   GNU General Public License for more details.
+; 
+;   You should have received a copy of the GNU General Public License
+;   along with this program; if not, write to the Free Software
+;   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+;
 
-/* Generated file - do not edit */
+;  Generated file - do not edit
 
 		.data
 		.align 32
