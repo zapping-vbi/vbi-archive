9b7be655ed2506600c87d2ca4dc10c3ad6ace41f
Michael H. Schimek <mschimek@users.sourceforge.net>
2003-03-06 21:56:27 +0000

    build_properties_contents: Fixed spurious semicolon in conditional.

--- zapping/src/properties.c
+++ zapping/src/properties.c
@@ -522,11 +522,11 @@ build_properties_contents	(GtkDialog	*dialog)
   register_widget(GTK_WIDGET(notebook), "properties-notebook");
 
   /* Put our logo when nothing is selected yet */
-  if ((logo = z_load_pixmap ("logo.png")));
-  {
-    gtk_notebook_append_page(notebook, logo, gtk_label_new(""));
-    page_count ++; /* No handler for this page */
-  }
+  if ((logo = z_load_pixmap ("logo.png")))
+    {
+      gtk_notebook_append_page(notebook, logo, gtk_label_new(""));
+      page_count ++; /* No handler for this page */
+    }
 
   gtk_widget_show_all(hbox);
 
