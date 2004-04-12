16e66b098768a4fc512470f6e249fb8c2e737e3b
Michael H. Schimek <mschimek@users.sourceforge.net>
2004-04-12 00:41:58 +0000

    *** empty log message ***

--- web/htdocs/src/parser_carsten.c
+++ web/htdocs/src/parser_carsten.c
@@ -2,6 +2,7 @@
  * Simple XML->HTML converter for the web page in http://zapping.sf.net
  * Carsten theme.
  * (C) 2001 Iñaki García Etxebarria
+ * -test-
  */
 #include <stdio.h>
 #include <stdlib.h>
