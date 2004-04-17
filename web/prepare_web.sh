cdb31dfe7a77d582e6023d9a27183c36ad08c54b
Michael H. Schimek <mschimek@users.sourceforge.net>
2004-04-17 06:23:05 +0000

    *** empty log message ***

--- web/prepare_web.sh
+++ web/prepare_web.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#$Id: prepare_web.sh,v 1.7 2004-04-17 05:53:24 mschimek Exp $
+#$Id: prepare_web.sh,v 1.8 2004-04-17 06:23:05 mschimek Exp $
 #
 # Checks our html pages out of cvs, puts the files online
 # and cleans up.
@@ -8,19 +8,25 @@
 # cd /home/groups/z/za/zapping
 # ./prepare_web.sh
 
-chmod ug=rwX,o-rwx . -R
+for file in *; do chmod ug=rwX,o-rwx $file -R; done
 umask 007
 cvs -z3 update -dP
+
 cvs -z3 -d:pserver:anonymous@cvs1:/cvsroot/zapping co zapping/ChangeLog
 mv zapping/ChangeLog htdocs/
 chmod a+rX htdocs/ChangeLog
 rm -fR zapping
-#chmod a+rX . htdocs
+
+chmod a+rX cgi-bin
+cd cgi-bin
+chmod a+rx printenv testenv
+cd -
+
 chmod a+rX htdocs
 cd htdocs
 chmod a+r *.php *.inc *.html *.jpeg *.gif *.png bookmark.ico rescd.zip
 for i in images_* screenshots; do
   find $i -name "CVS" -prune -o -exec chmod a+rX '{}' ';'
 done
-#chmod a+rX doc -R
+chmod a+rX doc -R
 cd -
