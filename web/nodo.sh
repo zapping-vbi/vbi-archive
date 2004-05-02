607c6a9b240a3941ab81e0c928c17e3fa3adf3dc
Michael H. Schimek <mschimek@users.sourceforge.net>
2004-05-02 02:32:27 +0000

    *** empty log message ***

--- web/nodo.sh
+++ web/nodo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: nodo.sh,v 1.3 2004-05-02 02:15:58 mschimek Exp $
+# $Id: nodo.sh,v 1.4 2004-05-02 02:32:27 mschimek Exp $
 #
 # Execute a command as user nobody.
 # Be careful what you do, this creates a temporary CGI script.
@@ -8,21 +8,22 @@
 # cd /home/groups/z/za/zapping
 # ./nodo.sh "whoami; pwd"
 
-# Trace execution, abort on error.
-set -e -x
+# Trace execution.
+set -x
 
 script=cgi-bin/nobody-temp-$$
 
-umask 666
+umask 077 || exit 1
 
 # cat <<here not possible
-echo '#!/bin/sh' >$script
-echo 'echo Content-type: text/plain' >>$script
-echo 'echo' >>$script
-echo '(' $* ') 2>&1' >>$script
-
+(
+echo '#!/bin/sh'
+echo 'echo Content-type: text/plain'
+echo 'echo'
+echo '(' $* ') 2>&1'
+) >$script
 chmod a+rx $script
 
-wget -q --proxy=off -O - http://$HOSTNAME/$script
+while ! wget -q --proxy=off -O - http://zapping.sourceforge.net/$script; do true; done
 
 rm cgi-bin/nobody-temp*
