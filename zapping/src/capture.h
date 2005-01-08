9fcf9fc4865f06b9c7014f929aca5174aade331b
Michael H. Schimek <mschimek@users.sourceforge.net>
2005-01-08 14:27:34 +0000

    Rewrote the capture format negotiation routines. Now clients can pass a set of suitable pixel formats. That makes on the fly changes a lot easier, badly needed by the deinterlace plugin, and allows more efficient buffer management.

--- zapping/src/capture.h
+++ zapping/src/capture.h
@@ -21,126 +21,54 @@
 
 #include "common/fifo.h"
 #include "tveng.h"
-
 #include "x11stuff.h"
 #include "zimage.h"
 
-/* Startup/shutdown of the modules */
-void startup_capture (void);
-void shutdown_capture (void);
-
-/* Description of a possible capture format */
-typedef struct {
-  gboolean	locked; /* TRUE if we need a fixed size */
-  gint		width, height; /* Frame size, only applies if locked */
-  tv_pixfmt pixfmt; /* The actual format */
-} capture_fmt;
-
-/*
- * Capture routines proper.
- */
-/*
- * Start capturing, returns -1 on error.
- */
-gint capture_start (tveng_device_info *info, GtkWidget *window);
-
-/*
- * Stop the current capture.
- */
-void capture_stop (void);
-
-/*
- * Requests the given format. Upon success a request id will be
- * returned, failure is signalled by returning -1. Once a format is
- * granted, you are assured to get all data read from the input device
- * in this format.
- */
-gint request_capture_format (capture_fmt *fmt);
-
-/*
- * Like request_capture_format, but when a request that isn't
- * compatible with this one arrives, the format will be temporarily
- * dropped. Useful when getting frames in this format would be nice
- * but it isn't absolutely essential.
- */
-gint suggest_capture_format (capture_fmt *fmt);
-
-/*
- * Releases the given requests, call this when you won't longer need
- * the format. The id returned by request_capture_format must be passed.
- */
-void release_capture_format (gint id);
-
-/*
- * Gets a list of the current granted requests.
- * @formats: Pointer to a place where to allocate the list, you must
- * g_free it when no longer needed.
- * @num_formats: Pointer to a place where to store the size of the list.
- */
-void get_request_formats (capture_fmt **fmts, gint *num_fmts);
-
-/*
- * Capture events:
- *	CAPTURE_STOP: No more data will be produced (for example, this
- *	will be called when capture is stopped through the
- *	GUI). Assume that notify hooks will be disconnected and no
- *	more data will arrive. The request list is also cleared, so
- *	free any data associated with it.
- *	CAPTURE_CHANGED: The internal capture format has
- *	changed. Typical usage is renegotiating the current capture
- *	format when this is received, perhaps a better format can be
- *	granted now.
- */
 typedef enum {
-  CAPTURE_STOP,
-  CAPTURE_CHANGE
-} capture_event;
-
-typedef void (*CaptureNotify) (capture_event	event,
-			       gpointer		user_data);
-
-/*
- * Connect to this if you want to get notified of capture events. You
- * can use the returned id when releasing the handler. Note that
- * handlers are automatically released when the capture is stopped,
- * but it's good practise to release them asap.
- * -1 is returned upon error.
- */
-gint add_capture_handler (CaptureNotify notify, gpointer user_data);
-
-/* Removes a handler, use the id returned by add_capture_handler */
-void remove_capture_handler (gint id);
-
-/*
- * The capture fifo.
- */
-extern zf_fifo capture_fifo;
-
-extern zf_fifo display_fifo;
-
-/*
- * Returns the zimage with the given pixformat contained in the
- * frame. NULL if it ain't possible to produce the zimage.
- * The returned zimage, if any, will only be valid until you
- * send_empty the buffer. Also, you shouldn't free the image, it's a
- * reference to an static object.
- */
-zimage *
-retrieve_frame			(capture_frame *	frame,
-				 tv_pixfmt		pixfmt,
-				 gboolean		copy);
-tv_pixfmt
-native_capture_format		(void);
+  /* Must not change image size. */
+  REQ_SIZE		= (1 << 0),
+  /* Must not change pixel format. */
+  REQ_PIXFMT		= (1 << 1),
+} req_flags;
 
 typedef guint
 display_filter_fn		(zimage *		dst0,
 				 zimage *		dst1);
-gboolean
+
+extern zf_fifo			capture_fifo;
+
+extern zimage *
+retrieve_frame			(capture_frame *	frame,
+				 tv_pixfmt		pixfmt,
+				 gboolean		copy);
+extern gboolean
 remove_display_filter		(display_filter_fn *	filter);
-gboolean
+extern gboolean
 add_display_filter		(display_filter_fn *	filter,
 				 tv_pixfmt		fmt,
 				 guint			width,
 				 guint			height);
+extern gboolean
+capture_stop			(void);
+extern gboolean
+capture_start			(tveng_device_info *	info,
+				 GtkWidget *		window);
+extern void
+release_capture_format		(gint			id);
+extern gboolean
+get_capture_format		(gint			id,
+				 guint *		width,
+				 guint *		height,
+				 tv_pixfmt *		pixfmt);
+extern gint
+request_capture_format		(tveng_device_info *	info,
+				 guint			width,
+				 guint			height,
+				 tv_pixfmt_set		pixfmt_set,
+				 req_flags		flags);
+extern void
+shutdown_capture		(void);
+extern void
+startup_capture			(void);
 
 #endif
