f2f874c4f600a5bd02bc4cdae3f7d9e29ce86974
Michael H. Schimek <mschimek@users.sourceforge.net>
2005-02-25 18:13:01 +0000

    Added cpu_detection_altivec() proto.

--- zapping/libtv/avec/avec.h
+++ zapping/libtv/avec/avec.h
@@ -2,7 +2,11 @@
 #define AVEC_H
 
 #include "libtv/misc.h"
+#include "libtv/cpu.h"
 
 extern clear_block_fn *		clear_block_altivec [4];
+
+extern cpu_feature_set
+cpu_detection_altivec		(void);
 
 #endif /* AVEC_H */
