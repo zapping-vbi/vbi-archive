2c8ff0e8c9019b94858e9ea001d0492baea00295
Michael H. Schimek <mschimek@users.sourceforge.net>
2005-05-10 01:11:26 +0000

    *** empty log message ***

--- web/backup.sh
+++ web/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: backup.sh,v 1.9 2005-05-10 01:03:35 mschimek Exp $
+# $Id: backup.sh,v 1.10 2005-05-10 01:11:26 mschimek Exp $
 #
 # Back up TWiki runtime data, supposed to be executed daily.
 
@@ -25,19 +25,13 @@ today=`date "+%Y%m%d-%H%M"`
 
 if test -d twiki.bak; then
   if test x`date +%d` = x27; then
-    tar -ch twiki | bzip2 -c >$here/twiki-$today.tar.bz2
-    cat $here/twiki-$today.tar.bz2 | \
-      uuencode --base64 twiki-$today.tar.bz2 | \
-      mail -s twiki-tar $send_backups_to
+    tar -ch twiki | bzip2 -c | \
+    uuencode --base64 twiki-$today.tar.bz2 | \
+    mail -s twiki-tar $send_backups_to
   else
-    diff -Nadru twiki.bak twiki >$here/twiki-$today.diff
-    if test -s $here/twiki-$today.diff ; then
-      bzip2 $here/twiki-$today.diff
-      cat $here/twiki-$today.diff.bz2 | \
-        uuencode --base64 twiki-$today.diff.bz2 | \
-        mail -s twiki-diff $send_backups_to
-      rm $here/twiki-$today.diff.bz2
-    fi
+    diff -Nadru twiki.bak twiki | bzip2 -c | \
+    uuencode --base64 twiki-$today.diff.bz2 | \
+    mail -s twiki-diff $send_backups_to
   fi
 
   rm -rf twiki.bak
