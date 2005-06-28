e60bb23a041afb724ee229f97bf41243df8680e3
Michael H. Schimek <mschimek@users.sourceforge.net>
2005-06-28 00:52:13 +0000

    load_options: Skip hidden options, for GreedyTestMode.

--- zapping/plugins/deinterlace/preferences.c
+++ zapping/plugins/deinterlace/preferences.c
@@ -18,7 +18,7 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-/* $Id: preferences.c,v 1.2 2005-02-05 22:21:43 mschimek Exp $ */
+/* $Id: preferences.c,v 1.3 2005-06-28 00:52:13 mschimek Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
@@ -344,6 +344,12 @@ create_option_table		(const DEINTERLACE_METHOD *method)
 
       key = NULL;
 
+      if (!setting->szDisplayName)
+	{
+	  /* Hidden option. */
+	  continue;
+	}
+
       switch (setting->Type)
 	  {
 	  case ONOFF:
@@ -663,7 +669,7 @@ create_change_set		(void)
 			   "   <type>%u</type>\n"
 			   "   <default>%ld</default>\n"
 			   "  </schema>\n",
-			   key, key, setting->Type, setting->Default);
+			   key, key, setting->Type, (long) setting->Default);
 
 		if (keys_size + 1 >= keys_capacity)
 		  {
